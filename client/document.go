9d5b7c36fa5ac0d2d7a6d080be3086f4b63545d1
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-05-12 23:01:17 +0100

    docs(client): correct spelling in comment

--- client/document.go
+++ client/document.go
@@ -1,2 +1,2 @@
-// Package authenticate provides methods to authenticate to various third party packages.
+// Package client provides methods to authenticate to various third party packages.
 package client // import "github.com/ministryofjustice/cloud-platform-go-library/client"
