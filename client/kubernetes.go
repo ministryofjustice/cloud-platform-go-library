a5416698c043a8bf0c83143bf61a9b8db133d621
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-05-12 23:02:54 +0100

    refactor(client): rewrite functions to amend KubeClient type, add comments for godoc

--- client/kubernetes.go
+++ client/kubernetes.go
@@ -3,7 +3,6 @@ package client
 import (
 	"fmt"
 	"io/ioutil"
-	"path/filepath"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -13,77 +12,49 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
 	"k8s.io/metrics/pkg/client/clientset/versioned"
 )
 
 // KubeClient is used to pass kubeconfig options
 // to methods that need to interact with the kubernetes api
 type KubeClient struct {
-	// ConfigPath is an optional filepath to a kubeconfig file.
+	// Path is an optional filepath to a kubeconfig file.
 	// Usually this is set to ~/.kube/config.
 	Path string
 	// Context is the name of the context to use in the kubeconfig file.
 	Context string
-	// Clientset is the kubernetes client set required to interact with the kubernetes api
+	// Clientset is the kubernetes client set required to interact with the kubernetes api.
 	Clientset kubernetes.Interface
+	// VersionedClientset allows you to communicate with the kubernetes api to get metrics data.
+	VersionedClientset versioned.Interface
 }
 
-// AWSOptions is used to pass aws options to methods that need them
-type AWSOptions struct {
+// AwsOptions is used to pass aws options to functions/methods that need them
+type AwsOptions struct {
+	// Profile relates to the AWS profile you'd set.
+	// In MoJ Cloud Platform this is standardised as "moj-cp".
 	Profile string
-	Key     string
-	Secret  string
-	Region  string
-	Bucket  string
+	// Key refers to the AWS_ACCESS_KEY_ID, usually set in an environment variable.
+	Key string
+	// Secret is the AWS_SECRET_ACCESS_KEY for an AWS account.
+	Secret string
+	// Region relates to the AWS Region you wish to use. The default for MoJ
+	// Cloud Platform should be "eu-west-2".
+	Region string
+	// Bucket name that holds credential file, if you wish to use one.
+	Bucket string
 }
 
-func NewKubeClient(path string) *KubeClient {
-	if path == "" {
-		path = filepath.Join(homedir.HomeDir(), ".kube", "config")
+// NewKubeClientWithValues takes the path of a kubeconfig file and a Kubernetes context
+// as a string and returns a populated KubeClient type with the Kubernetes Clientset.
+func NewKubeClientWithValues(configFilePath, context string) (*KubeClient, error) {
+	// You can still build with empty strings here, but you may not get far.
+	client := &KubeClient{
+		Path:    configFilePath,
+		Context: context,
 	}
 
-	return &KubeClient{
-		Path:      path,
-		Clientset: &kubernetes.Clientset{},
-	}
-}
-
-func (kube *KubeClient) ClientsetWithCurrentContext() (kubernetes.Interface, error) {
-	config, err := clientcmd.BuildConfigFromFlags("", kube.Path)
-	if err != nil {
-		return nil, err
-	}
-
-	return createClientSet(config)
-}
-
-func (kube *KubeClient) ClientSetWithContext() (kubernetes.Interface, error) {
-	client, err := kube.NewConfigFromContext()
-	if err != nil {
-		return nil, err
-	}
-
-	return createClientSet(client)
-}
-
-func (kube *KubeClient) MetricsClientSetWithContext() (*versioned.Clientset, error) {
-	client, err := kube.NewConfigFromContext()
-	if err != nil {
-		return nil, err
-	}
-
-	return versioned.NewForConfig(client)
-}
-
-func (kube *KubeClient) CreateClientSetFromS3(filepath string, awsOpt AWSOptions) (kubernetes.Interface, error) {
-	// Ignore the kubeconfig filepath variable and download the config from s3
-	_, err := kube.DownloadS3Kubeconfig(filepath, awsOpt)
-	if err != nil {
-		return nil, err
-	}
-
-	client, err := kube.ClientsetWithCurrentContext()
+	err := client.BuildClientSet()
 	if err != nil {
 		return nil, err
 	}
@@ -91,6 +62,93 @@ func (kube *KubeClient) CreateClientSetFromS3(filepath string, awsOpt AWSOptions
 	return client, nil
 }
 
+// NewMetricsKubeClientWithValues takes the path of a kubeconfig file and a Kubernetes context
+// and returns a populated KubeClient type with the Kubernetes versioned.Clientset, which allows you
+// to communicate with the kube metrics api.
+func NewMetricsKubeClientWithValues(configFilePath, context string) (*KubeClient, error) {
+	// You can still build with empty strings here, but you may not get far.
+	client := &KubeClient{
+		Path:    configFilePath,
+		Context: context,
+	}
+
+	err := client.BuildVersionedClientset()
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+// BuildClientSet method builds a clientset depending on whether
+// the KubeClient type has a non empty Context defined.
+// If defined the clientset will have the context set to its value.
+func (kube *KubeClient) BuildClientSet() (err error) {
+	var config *rest.Config
+	if kube.Context == "" {
+		config, err = clientcmd.BuildConfigFromFlags("", kube.Path)
+		if err != nil {
+			return err
+		}
+	} else {
+		config, err = kube.NewConfigFromContext()
+		if err != nil {
+			return err
+		}
+	}
+
+	kube.Clientset, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// BuildClientSet method builds a versioned clientset depending on whether
+// the KubeClient type has a non empty Context defined.
+// If defined the clientset will have the context set to its value.
+// A versioned clientset is used to communicate with the metrics api.
+func (kube *KubeClient) BuildVersionedClientset() (err error) {
+	var config *rest.Config
+	if kube.Context == "" {
+		config, err = clientcmd.BuildConfigFromFlags("", kube.Path)
+		if err != nil {
+			return err
+		}
+	} else {
+		config, err = kube.NewConfigFromContext()
+		if err != nil {
+			return err
+		}
+	}
+
+	kube.VersionedClientset, err = versioned.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// BuildClientSetFromS3 takes a string representing the kubeconfig file to download.
+// The method uses an AwsOptions type to download said kubeconfig to the path set by the KubeClient. It
+// then builds and sets a clientset.
+func (kube *KubeClient) BuildClientSetFromS3(filepath string, awsOpt AwsOptions) error {
+	_, err := DownloadS3Kubeconfig(filepath, kube.Path, awsOpt)
+	if err != nil {
+		return err
+	}
+
+	err = kube.BuildClientSet()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// NewConfigFromContext returns a config type ready to create a Kubernetes interface.
 func (kube *KubeClient) NewConfigFromContext() (*rest.Config, error) {
 	if kube.Path == "" || kube.Context == "" {
 		return nil, fmt.Errorf("Config file and Context must be set")
@@ -103,10 +161,8 @@ func (kube *KubeClient) NewConfigFromContext() (*rest.Config, error) {
 		}).ClientConfig()
 }
 
-func createClientSet(r *rest.Config) (kubernetes.Interface, error) {
-	return kubernetes.NewForConfig(r)
-}
-
+// SwitchKubeContext takes the KubeClient context type and ensures it
+// is set appropriately.
 func (kube *KubeClient) SwitchKubeContext() error {
 	kubeconf := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
 		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kube.Path},
@@ -132,7 +188,10 @@ func (kube *KubeClient) SwitchKubeContext() error {
 	return nil
 }
 
-func (kube *KubeClient) DownloadS3Kubeconfig(fileName string, awsOpt AWSOptions) (string, error) {
+// DownloadS3Kubeconfig takes a file name, the path of a kubeconfig file and an AwsOptions type.
+// It will download the fileName in the awsOpts.Bucket and create a kubeconfig file in the path
+// specified. The function returns the location of the newly created kubconfig.
+func DownloadS3Kubeconfig(fileName, kubeconfig string, awsOpt AwsOptions) (string, error) {
 	if awsOpt.Key == "" && awsOpt.Secret == "" || awsOpt.Profile == "" {
 		return "", fmt.Errorf("AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY or AWS_PROFILE must be set")
 	}
@@ -165,10 +224,10 @@ func (kube *KubeClient) DownloadS3Kubeconfig(fileName string, awsOpt AWSOptions)
 	}
 
 	data := buff.Bytes()
-	err = ioutil.WriteFile(kube.Path, data, 0o644)
+	err = ioutil.WriteFile(kubeconfig, data, 0o644)
 	if err != nil {
 		return "", err
 	}
 
-	return kube.Path, nil
+	return kubeconfig, nil
 }
--- client/kubernetes_test.go
+++ client/kubernetes_test.go
@@ -1,16 +1,236 @@
 package client
 
 import (
+	"log"
 	"os"
 	"reflect"
 	"testing"
 
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/fake"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"k8s.io/client-go/rest"
 	"k8s.io/metrics/pkg/client/clientset/versioned"
 )
+
+var file *os.File
+
+func TestMain(m *testing.M) {
+	var err error
+	file, err = createMockKubeConfigFile("temp")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	code := m.Run()
+
+	defer file.Close()
+	defer os.Remove(file.Name())
+	os.Exit(code)
+}
+
+func TestNewKubeClientWithValues(t *testing.T) {
+	type args struct {
+		configFilePath string
+		context        string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "Create new client with empty context",
+			args: args{
+				configFilePath: file.Name(),
+				context:        "",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Create new client with existing context",
+			args: args{
+				configFilePath: file.Name(),
+				context:        "kind-kind",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Create new client with fake context",
+			args: args{
+				configFilePath: file.Name(),
+				context:        "fake",
+			},
+			wantErr: true,
+		},
+		{
+			name: "Create new client with incorrect file path",
+			args: args{
+				configFilePath: "fake",
+				context:        "",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewKubeClientWithValues(tt.args.configFilePath, tt.args.context)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewKubeClientWithValues() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := NewMetricsKubeClientWithValues(tt.args.configFilePath, tt.args.context)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewMetricsKubeClientWithValues() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
+
+func TestKubeClient_BuildClientSet(t *testing.T) {
+	type fields struct {
+		Path               string
+		Context            string
+		Clientset          kubernetes.Interface
+		VersionedClientset versioned.Interface
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "Build clientset with correct data",
+			fields: fields{
+				Path:    file.Name(),
+				Context: "kind-kind",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Build clientset with incorrect data",
+			fields: fields{
+				Path:    file.Name(),
+				Context: "fake",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			kube := &KubeClient{
+				Path:               tt.fields.Path,
+				Context:            tt.fields.Context,
+				Clientset:          tt.fields.Clientset,
+				VersionedClientset: tt.fields.VersionedClientset,
+			}
+			if err := kube.BuildClientSet(); (err != nil) != tt.wantErr {
+				t.Errorf("KubeClient.BuildClientSet() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if err := kube.BuildVersionedClientset(); (err != nil) != tt.wantErr {
+				t.Errorf("KubeClient.VersionedClientset() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestKubeClient_NewConfigFromContext(t *testing.T) {
+	type fields struct {
+		Path               string
+		Context            string
+		Clientset          kubernetes.Interface
+		VersionedClientset versioned.Interface
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    *rest.Config
+		wantErr bool
+	}{
+		{
+			name: "Create new config",
+			fields: fields{
+				Path:    file.Name(),
+				Context: "kind-kind",
+			},
+			want: &rest.Config{
+				Host:    "https://127.0.0.1:55171",
+				APIPath: "", ContentConfig: rest.ContentConfig{
+					AcceptContentTypes:   "",
+					ContentType:          "",
+					GroupVersion:         (*schema.GroupVersion)(nil),
+					NegotiatedSerializer: runtime.NegotiatedSerializer(nil),
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			kube := &KubeClient{
+				Path:               tt.fields.Path,
+				Context:            tt.fields.Context,
+				Clientset:          tt.fields.Clientset,
+				VersionedClientset: tt.fields.VersionedClientset,
+			}
+			got, err := kube.NewConfigFromContext()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("KubeClient.NewConfigFromContext() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("KubeClient.NewConfigFromContext() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestKubeClient_SwitchKubeContext(t *testing.T) {
+	type fields struct {
+		Path               string
+		Context            string
+		Clientset          kubernetes.Interface
+		VersionedClientset versioned.Interface
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "Switch the kube context using correct data",
+			fields: fields{
+				Path:    file.Name(),
+				Context: "kind-kind",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Switch the kube context using incorrect data",
+			fields: fields{
+				Path:    file.Name(),
+				Context: "fake",
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			kube := &KubeClient{
+				Path:               tt.fields.Path,
+				Context:            tt.fields.Context,
+				Clientset:          tt.fields.Clientset,
+				VersionedClientset: tt.fields.VersionedClientset,
+			}
+			if err := kube.SwitchKubeContext(); (err != nil) != tt.wantErr {
+				t.Errorf("KubeClient.SwitchKubeContext() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
 
 func createMockKubeConfigFile(path string) (*os.File, error) {
 	var data = []byte(`
@@ -51,288 +271,4 @@ users:
 	}
 
 	return file, nil
-}
-
-func TestNewKubeClient(t *testing.T) {
-	type args struct {
-		path string
-	}
-	tests := []struct {
-		name string
-		args args
-		want *KubeClient
-	}{
-		{
-			name: "Create a new kube client",
-			args: args{
-				path: "test",
-			},
-			want: &KubeClient{
-				Path:      "test",
-				Clientset: &kubernetes.Clientset{},
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewKubeClient(tt.args.path); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewKubeClient() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestKubeClient_ClientsetWithCurrentContext(t *testing.T) {
-	file, err := createMockKubeConfigFile("temp")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer file.Close()
-	defer os.Remove(file.Name())
-
-	type fields struct {
-		Path      string
-		Context   string
-		Clientset kubernetes.Interface
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
-	}{
-		{
-			name: "Create a new clientset with current context",
-			fields: fields{
-				Path:      file.Name(),
-				Context:   "kind-kind",
-				Clientset: fake.NewSimpleClientset(),
-			},
-			wantErr: false,
-		},
-		{
-			name: "Try with invalid path",
-			fields: fields{
-				Path:      "test",
-				Context:   "test",
-				Clientset: fake.NewSimpleClientset(),
-			},
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			kube := &KubeClient{
-				Path:      tt.fields.Path,
-				Context:   tt.fields.Context,
-				Clientset: tt.fields.Clientset,
-			}
-			_, err := kube.ClientsetWithCurrentContext()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("KubeClient.ClientsetWithCurrentContext() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-		})
-	}
-}
-
-func TestKubeClient_ClientSetWithContext(t *testing.T) {
-	file, err := createMockKubeConfigFile("temp")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	defer file.Close()
-	defer os.Remove(file.Name())
-	type fields struct {
-		Path      string
-		Context   string
-		Clientset kubernetes.Interface
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
-	}{
-		{
-			name: "Create a new clientset with new context",
-			fields: fields{
-				Path:      file.Name(),
-				Context:   "kind-kind2",
-				Clientset: fake.NewSimpleClientset(),
-			},
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			kube := &KubeClient{
-				Path:      tt.fields.Path,
-				Context:   tt.fields.Context,
-				Clientset: tt.fields.Clientset,
-			}
-			_, err := kube.ClientSetWithContext()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("KubeClient.ClientSetWithContext() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-		})
-	}
-}
-
-func TestKubeClient_MetricsClientSetWitContext(t *testing.T) {
-	type fields struct {
-		Path      string
-		Context   string
-		Clientset kubernetes.Interface
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		want    *versioned.Clientset
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			kube := &KubeClient{
-				Path:      tt.fields.Path,
-				Context:   tt.fields.Context,
-				Clientset: tt.fields.Clientset,
-			}
-			got, err := kube.MetricsClientSetWithContext()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("KubeClient.MetricsClientSetWitContext() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("KubeClient.MetricsClientSetWitContext() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestKubeClient_CreateClientSetFromS3(t *testing.T) {
-	type fields struct {
-		Path      string
-		Context   string
-		Clientset kubernetes.Interface
-	}
-	type args struct {
-		filepath string
-		awsOpt   AWSOptions
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    kubernetes.Interface
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			kube := &KubeClient{
-				Path:      tt.fields.Path,
-				Context:   tt.fields.Context,
-				Clientset: tt.fields.Clientset,
-			}
-			got, err := kube.CreateClientSetFromS3(tt.args.filepath, tt.args.awsOpt)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("KubeClient.CreateClientSetFromS3() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("KubeClient.CreateClientSetFromS3() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestKubeClient_NewConfigFromContext(t *testing.T) {
-	type fields struct {
-		Path      string
-		Context   string
-		Clientset kubernetes.Interface
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		want    *rest.Config
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			kube := &KubeClient{
-				Path:      tt.fields.Path,
-				Context:   tt.fields.Context,
-				Clientset: tt.fields.Clientset,
-			}
-			got, err := kube.NewConfigFromContext()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("KubeClient.NewConfigFromContext() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("KubeClient.NewConfigFromContext() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_createClientSet(t *testing.T) {
-	type args struct {
-		r *rest.Config
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    kubernetes.Interface
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := createClientSet(tt.args.r)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("createClientSet() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("createClientSet() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestKubeClient_SwitchKubeContext(t *testing.T) {
-	type fields struct {
-		Path      string
-		Context   string
-		Clientset kubernetes.Interface
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			kube := &KubeClient{
-				Path:      tt.fields.Path,
-				Context:   tt.fields.Context,
-				Clientset: tt.fields.Clientset,
-			}
-			if err := kube.SwitchKubeContext(); (err != nil) != tt.wantErr {
-				t.Errorf("KubeClient.SwitchKubeContext() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
 }
