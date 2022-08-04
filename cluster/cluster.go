3b030006f5f5682c6a682adcc204de831900b0e4
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-08-04 15:45:30 +0100

    feat(cluster): add create options struct and methods

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -16,6 +16,30 @@ type Cluster struct {
 	Namespaces v1.NamespaceList
 }
 
+// CreateOptions struct represents the options passed to the Create method.
+type CreateOptions struct {
+	Name          string
+	ClusterSuffix string
+
+	NodeCount int
+	VpcName   string
+
+	Kubeconfig string
+
+	MaxNameLength int `default:"12"`
+	TimeOut       int
+	Debug         bool
+
+	Auth0          AuthOpts
+	AwsCredentials client.AwsCredentials
+}
+
+type AuthOpts struct {
+	Domain       string
+	ClientId     string
+	ClientSecret string
+}
+
 // NewWithValues returns a full Cluster object with populated values.
 func NewWithValues(c client.KubeClient) (*Cluster, error) {
 	nodes, err := AllNodes(c)
@@ -45,4 +69,53 @@ func NewWithValues(c client.KubeClient) (*Cluster, error) {
 // GetName is a method function to get the name of the cluster.
 func (c *Cluster) GetName() {
 	c.Name = c.Nodes.Items[0].Labels["Cluster"]
+}
+
+// Create creates a new Kubernetes cluster using the options passed to it.
+func (c *Cluster) Create(opts *CreateOptions) error {
+	// create vpc
+	err := createVpc(opts)
+	if err != nil {
+		return err
+	}
+
+	// create kubernetes cluster
+	err = createCluster(opts)
+	if err != nil {
+		return err
+	}
+
+	// install components into kubernetes cluster
+	err = installComponents(opts)
+	if err != nil {
+		return err
+	}
+
+	// perform health check on the cluster
+	err = healthCheck(opts)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// createVpc creates a new VPC in AWS.
+func createVpc(opts *CreateOptions) error {
+	return nil
+}
+
+// CreateCluster creates a new Kubernetes cluster in AWS.
+func createCluster(opts *CreateOptions) error {
+	return nil
+}
+
+// InstallComponents installs components into the Kubernetes cluster.
+func installComponents(opts *CreateOptions) error {
+	return nil
+}
+
+// HealthCheck performs a health check on the Kubernetes cluster.
+func healthCheck(opts *CreateOptions) error {
+	return nil
 }
