6682945fde68f5854029160a0b3443764b84c04e
jasonBirchall <jason.birchall@digital.justice.gov.uk>
2022-08-08 21:17:42 +0100

    fix(git): merge conflict resolution and dir lookup

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -1,6 +1,21 @@
 package cluster
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/go-version"
+	install "github.com/hashicorp/hc-install"
+	"github.com/hashicorp/hc-install/fs"
+	"github.com/hashicorp/hc-install/product"
+	"github.com/hashicorp/hc-install/releases"
+	"github.com/hashicorp/hc-install/src"
+	"github.com/hashicorp/terraform-exec/tfexec"
 	"github.com/ministryofjustice/cloud-platform-go-library/client"
 	"github.com/pkg/errors"
 	v1 "k8s.io/api/core/v1"
@@ -25,14 +40,14 @@ type CreateOptions struct {
 	NodeCount int
 	VpcName   string
 
-	Kubeconfig string
-
-	MaxNameLength int `default:"12"`
+	MaxNameLength int
 	TimeOut       int
 	Debug         bool
 
 	Auth0          AuthOpts
 	AwsCredentials client.AwsCredentials
+
+	Logger log.Logger
 }
 
 type AuthOpts struct {
@@ -93,6 +108,8 @@ func findTopLevelGitDir(workingDir string) (string, error) {
 
 // Create creates a new Kubernetes cluster using the options passed to it.
 func (c *Cluster) Create(opts *CreateOptions) error {
+	c.Name = opts.Name
+
 	repoName, err := findTopLevelGitDir(".")
 	if err != nil {
 		return err
@@ -102,8 +119,27 @@ func (c *Cluster) Create(opts *CreateOptions) error {
 		return errors.New("must be run from the cloud-platform-infrastructure repository")
 	}
 
+	fmt.Printf("Creating cluster %s", c.Name)
+	i := install.NewInstaller()
+
+	v0_14_8 := version.Must(version.NewVersion("0.14.8"))
+
+	fmt.Println("Installing terraform")
+	execPath, err := i.Ensure(context.Background(), []src.Source{
+		&fs.ExactVersion{
+			Product: product.Terraform,
+			Version: v0_14_8,
+		},
+		&releases.ExactVersion{
+			Product: product.Terraform,
+			Version: v0_14_8,
+		},
+	})
+
+	defer i.Remove(context.Background())
+
 	// create vpc
-	err = c.createVpc(opts)
+	err = c.CreateVpc(opts, execPath)
 	if err != nil {
 		return err
 	}
@@ -130,7 +166,102 @@ func (c *Cluster) Create(opts *CreateOptions) error {
 }
 
 // createVpc creates a new VPC in AWS.
-func (c *Cluster) createVpc(opts *CreateOptions) error {
+func (c *Cluster) CreateVpc(opts *CreateOptions, execPath string) error {
+	fmt.Println("Checking out tf dir")
+	workingDir := "terraform/aws-accounts/cloud-platform-aws/vpc"
+	tf, err := tfexec.NewTerraform(workingDir, execPath)
+	if err != nil {
+		log.Fatalf("error running NewTerraform: %s", err)
+	}
+
+	// if .terraform.tfstate directory exists, delete it
+	fmt.Println("Deleting .terraform.tfstate directory")
+	if _, err := os.Stat("terraform/aws-accounts/cloud-platform-aws/vpc/.terraform"); err == nil {
+		err = os.RemoveAll("terraform/aws-accounts/cloud-platform-aws/vpc/.terraform")
+		if err != nil {
+			return err
+		}
+	}
+
+	fmt.Println("Performing a terraform init")
+	err = tf.Init(context.Background())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Creating a new workspace")
+	err = tf.WorkspaceNew(context.Background(), c.Name)
+	if err != nil {
+		return err
+	}
+
+	ws, err := tf.WorkspaceShow(context.Background())
+	fmt.Println("Applying in workspace", ws)
+	planPath := fmt.Sprintf("%s/%s-%v", "./", "plan", time.Now().Unix())
+	planOptions := []tfexec.PlanOption{
+		tfexec.Out(planPath),
+		tfexec.Refresh(true),
+		tfexec.Parallelism(1),
+	}
+	_, err = tf.Plan(context.Background(), planOptions...)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Generating plan file")
+	plan, err := tf.ShowPlanFile(context.Background(), planPath)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Make this a debug option
+	for k, v := range plan.OutputChanges {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+
+	fmt.Println("Applying plan")
+	statePath := fmt.Sprintf("%s/%s-%v", "./", "state", time.Now().Unix())
+	applyOptions := []tfexec.ApplyOption{
+		tfexec.DirOrPlan(planPath),
+		tfexec.StateOut(statePath),
+		tfexec.Parallelism(1),
+	}
+	err = tf.Apply(context.Background(), applyOptions...)
+	if err != nil {
+		return err
+	}
+
+	// Show the final terraform
+	fmt.Println("Showing final state")
+	state, err := tf.ShowStateFile(context.Background(), statePath)
+	if err != nil {
+		return err
+	}
+
+	for k, v := range state.Values.Outputs {
+		fmt.Printf("%s: %v\n", k, v)
+	}
+
+	destroyOptions := []tfexec.DestroyOption{
+		tfexec.Refresh(true),
+		tfexec.Parallelism(1),
+	}
+
+	err = tf.Destroy(context.Background(), destroyOptions...)
+	if err != nil {
+		return err
+	}
+
+	err = tf.WorkspaceSelect(context.Background(), "default")
+	if err != nil {
+		return err
+	}
+
+	err = tf.WorkspaceDelete(context.Background(), c.Name)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
